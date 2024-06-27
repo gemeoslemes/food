a8de98de5f2f5c1eefc319670e9a64c0e9d0175d
gemeoslemes <victorlemes0776@gmail.com>
2024-06-27 00:33:23 -0300

    Criando a entidade categorias

--- src/main/java/br/com/food/domain/Categorias.java
+++ src/main/java/br/com/food/domain/Categorias.java
@@ -1,4 +1,62 @@
 package br.com.food.domain;
 
-public class Categorias {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categorias")
+public class Categorias implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    public Categorias() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categorias that = (Categorias) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(descricao, that.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, descricao);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
